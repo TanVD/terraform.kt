498b54ba768c004c777909a299a0fb5992c69fb8
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2020-08-26 23:09:34 +0300

    generate files instead of using stdout

--- src/main/kotlin/io/terraformkt/Main.kt
+++ src/main/kotlin/io/terraformkt/Main.kt
@@ -2,28 +2,50 @@ package io.terraformkt
 
 import com.squareup.kotlinpoet.*
 import io.terraformkt.utils.Json
+import io.terraformkt.utils.Text.snakeToCamelCase
 import java.io.File
+
+const val baseDirectoryName = "generated"
+const val resourcesDirectoryName = "resource_schemas"
+const val dataDirectoryName = "data_source_schemas"
 
 fun main() {
     val jsonString = File("src/main/resources/schema.json").inputStream().readBytes().toString(Charsets.UTF_8)
     val schema = Json.parse<Schema>(jsonString)
-    val attributes = schema.provider_schemas.aws.resource_schemas.aws_lambda_function.block.attributes
-   // println(attributes)
-
-    val lambdaFunction = TypeSpec.classBuilder("LambdaFunction")
-    for (attr in attributes) {
-        if (attr.value.containsKey("type") && attr.value["type"] is String) {
-            val type = attr.value["type"] as String
-            var isComputed = false
-            if (attr.value.containsKey("computed")) {
-                isComputed = attr.value["computed"] as Boolean
-            }
-            lambdaFunction.addProperty(PropertySpec.builder(attr.key, typeToKotlinType(type))
-                .delegate(typeToDelegate(type, isComputed)).build())
-        }
+    val generatedDirectory = File(baseDirectoryName)
+    if (!generatedDirectory.exists()) {
+        generatedDirectory.mkdir()
     }
 
-    println(lambdaFunction.build())
+    val resources = schema.provider_schemas.aws.resource_schemas
+    val data = schema.provider_schemas.aws.data_source_schemas
+    generateFiles(resourcesDirectoryName, resources)
+    generateFiles(dataDirectoryName, data)
+}
+
+fun generateFiles(path: String, resources: Map<String, Resource>) {
+    val directory = File("$baseDirectoryName/$path")
+    if (!directory.exists()) {
+        directory.mkdir()
+    }
+    for (resourceName in resources.keys) {
+        val className = snakeToCamelCase(resourceName)
+        val resourceClass = TypeSpec.classBuilder(className)
+        for (attr in resources[resourceName]!!.block.attributes) {
+            if (attr.value.containsKey("type") && attr.value["type"] is String) {
+                val type = attr.value["type"] as String
+                var isComputed = false
+                if (attr.value.containsKey("computed")) {
+                    isComputed = attr.value["computed"] as Boolean
+                }
+                resourceClass.addProperty(
+                    PropertySpec.builder(attr.key, typeToKotlinType(type))
+                        .delegate(typeToDelegate(type, isComputed)).build()
+                )
+            }
+        }
+        File("$baseDirectoryName/$path/$className.kt").writeText(resourceClass.build().toString(), Charsets.UTF_8)
+    }
 }
 
 fun typeToDelegate(type: String, isComputed: Boolean): String {
@@ -33,11 +55,8 @@ fun typeToDelegate(type: String, isComputed: Boolean): String {
         "bool" -> "bool"
         else -> ""
     }
-    if (isComputed) {
-        delegateName += "(computed = true)"
-    } else {
-        delegateName += "()"
-    }
+    delegateName += if (isComputed) "(computed = true)" else "()"
+
     return delegateName
 }
 
--- src/main/kotlin/io/terraformkt/utils/Text.kt
+++ src/main/kotlin/io/terraformkt/utils/Text.kt
@@ -2,6 +2,7 @@ package io.terraformkt.utils
 
 object Text {
     private val camelCaseRegex = Regex("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])")
+    private val snakeCaseRegex = Regex("_[a-zA-Z]")
 
     fun decamelize(text: String): List<String> = text.split(camelCaseRegex)
     fun desnake(text: String): List<String> = text.split("_")
@@ -13,6 +14,13 @@ object Text {
         .flatMap { dehyphen(it) }
         .flatMap { dedot(it) }
         .filter { it.isNotBlank() }
+
+    fun snakeToCamelCase(text: String): String {
+        return snakeCaseRegex.replace(text) {
+            it.value.replace("_", "")
+                .toUpperCase()
+        }.capitalize()
+    }
 
     const val indent = 2
 
