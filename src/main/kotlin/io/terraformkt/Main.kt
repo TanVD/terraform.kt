1644194cb9d6e2797f414bf6a33780048b275064
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2020-08-26 12:51:38 +0300

    create class with Kotlin poet

--- src/main/kotlin/io/terraformkt/Main.kt
+++ src/main/kotlin/io/terraformkt/Main.kt
@@ -1,6 +1,6 @@
 package io.terraformkt
 
-import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.*
 import io.terraformkt.hcl.Json
 import io.terraformkt.hcl.Schema
 import java.io.File
@@ -8,10 +8,25 @@ import java.io.File
 fun main() {
     val jsonString = File("src/main/resources/schema.json").inputStream().readBytes().toString(Charsets.UTF_8)
     val schema = Json.parse<Schema>(jsonString)
-    println(schema.provider_schemas.aws.resource_schemas.aws_lambda_function.block.attributes)
+    val attributes = schema.provider_schemas.aws.resource_schemas.aws_lambda_function.block.attributes
+   // println(attributes)
 
-    val lambda = FunSpec.builder("lambda")
-        .returns(Char::class)
-        .addStatement("val ")
-        .build()
+    val lambdaFunction = TypeSpec.classBuilder("LambdaFunction")
+    for (attr in attributes) {
+        if (attr.value.containsKey("type") && attr.value["type"] is String) {
+            val type = attr.value["type"] as String
+            lambdaFunction.addProperty(PropertySpec.builder(attr.key, ANY).delegate(typeToDelegate(type)).build())
+        }
+    }
+
+    println(lambdaFunction.build())
+}
+
+fun typeToDelegate(type : String) : String {
+    return when(type){
+        "string" -> "text()"
+        "number" -> "int()"
+        "bool" -> "bool()"
+        else -> ""
+    }
 }
