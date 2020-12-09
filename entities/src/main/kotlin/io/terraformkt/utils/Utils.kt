e593e503d979ca6e575bc9a090390b6b2145085b
anstkras <anstkras@gmail.com>
2020-12-10 00:10:46 +0300

    render maps properly

--- entities/src/main/kotlin/io/terraformkt/utils/Utils.kt
+++ entities/src/main/kotlin/io/terraformkt/utils/Utils.kt
@@ -1,5 +1,7 @@
 package io.terraformkt.utils
 
+import io.terraformkt.hcl.HCLMapField
+
 /**
  * Unlink is used to restyle old `"${ref}"` style refs to Terraform 12 style `ref`
  */
@@ -12,3 +14,16 @@ fun isLink(field: String) = field.startsWith("\${") && field.endsWith("}")
 
 fun link(field: String) = "\${$field}"
 fun link(vararg parts: String?) = link(parts.filterNotNull().joinToString(separator = "."))
+
+fun toText(value: String) = if (isLink(value)) unlink(value) else "\"$value\""
+
+// Suppose any can be int, string, bool and map.
+fun anyToText(any: Any): String {
+    if (any is String) {
+        return toText(any)
+    }
+    if (any is Map<*, *>) {
+        return "{${HCLMapField.renderMap(any as Map<String, Any>)}}"
+    }
+    return "$any"
+}
