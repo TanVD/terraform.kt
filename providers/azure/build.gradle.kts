82b875ba76fcc2518a2ab80cb42e6c5805cfd936
Vladislav Tankov <vdtankov@gmail.com>
2021-09-20 12:31:52 +0300

    Update build.gradle.kts

--- providers/azure/build.gradle.kts
+++ providers/azure/build.gradle.kts
@@ -1,7 +1,7 @@
 import io.terraformkt.plugin.terraformKt
 import tanvd.kosogor.proxy.publishJar
 
-val providerVersion = "2.56.0"
+val providerVersion = "2.77.0"
 
 group = rootProject.group
 version = "$providerVersion-${rootProject.version}"
